8dee921aac534dff339e2b0a97926f39739d649f
Its33shubh <shubhamkakloter123@gmail.com>
2026-04-21 15:58:58 +0530

    make the register and login api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,137 @@
-// const User = require('../models/User')
-// const bcrypt = require('bcryptjs')
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-// exports.Register = async (req,res) => {
-//     try {
-//         const {name,email,password,phone} = req.body
+//  REGISTER 
+exports.Register = async (req, res) => {
+    try {
+        const { name, phone, email, password } = req.body;
 
-//     } catch (error) {
-        
-//     }
-// }
+        if (!name || !phone || !email || !password) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+
+        const emailRegex = /^\S+@\S+\.\S+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email format"
+            })
+        }
+
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //user create
+        const user = await User.create({
+            name,
+            phone,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            user: {
+                id: user._id,
+                name: user.name,
+                phone: user.phone,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+exports.Login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "Email and password are required"
+            });
+        }
+        const emailRegex = /^\S+@\S+\.\S+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email format"
+            })
+        }
+
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "Invalid email"
+            });
+        }
+
+        // compare 
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "Invalid password"
+            });
+        }
+
+        // generate token
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.status(200).json({
+            error: false,
+            success: true,
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                phone: user.phone,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            error: true,
+            success: false,
+            message: error.message
+        });
+    }
+}
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const connectDB = require('./config/db')
+const authRoutes = require("./routes/authRoutes");
 
 const app = express()
 
 app.use(express.json())
 
 connectDB()
+
+app.use("/api/auth", authRoutes);
 
 app.get('/',(req,res)=>{
     res.send('app is run')
