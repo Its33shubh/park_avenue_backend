7b4e456491c42ee7a38422b5f4667a8bac866576
Its33shubh <shubhamkakloter123@gmail.com>
2026-04-21 13:16:42 +0530

    add the modul for user and connect the mongobd with node

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+// const User = require('../models/User')
+// const bcrypt = require('bcryptjs')
+
+// exports.Register = async (req,res) => {
+//     try {
+//         const {name,email,password,phone} = req.body
+
+//     } catch (error) {
+        
+//     }
+// }
