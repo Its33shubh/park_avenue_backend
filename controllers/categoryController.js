f337ec94e00df4078e450af5d300ff9482f80527
Its33shubh <shubhamkakloter123@gmail.com>
2026-04-23 15:29:43 +0530

    add delete category Api

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -107,3 +107,33 @@ exports.updateCategory = async (req, res) => {
   }
 }
 
+exports.deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(404).json({
+        error: true,
+        success:false,
+        message: "Category not found"
+      });
+    }
+
+    await Category.findByIdAndDelete(id);
+
+    res.json({
+      error:false,
+      success: true,
+      message: "Category deleted successfully"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      success:false,
+      message: error.message
+    });
+  }
+}
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const upload = require("../middleware/upload");
-const { addCategory,getCategories,updateCategory } = require("../controllers/categoryController");
+const { addCategory,getCategories,updateCategory,deleteCategory } = require("../controllers/categoryController");
 
 const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
@@ -10,5 +10,6 @@ const roleMiddleware = require("../middleware/roleMiddleware");
 router.post( "/add", authMiddleware, roleMiddleware("admin"), upload.single("image"), addCategory);
 router.get("/get", getCategories);
 router.put("/update/:id",authMiddleware,roleMiddleware("admin"),upload.single("image"),updateCategory);
+router.delete("/delete/:id",authMiddleware,roleMiddleware("admin"),deleteCategory);
 
 module.exports = router;
