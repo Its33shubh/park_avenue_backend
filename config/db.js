121f5ada63eaed5663f0732589231a4390e5ec91
Its33shubh <shubhamkakloter123@gmail.com>
2026-04-24 12:21:11 +0530

    update the db connection for make faster

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.time("MongoDB Connected");
     await mongoose.connect(process.env.MONGO_URI);
-
+    console.timeEnd("MongoDB Connected");
     console.log("MongoDB Connected");
 
   } catch (error) {
