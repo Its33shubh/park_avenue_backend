1f8fdad14950720e68305b6086505555980b41b8
Its33shubh <shubhamkakloter123@gmail.com>
2026-04-27 10:52:09 +0530

    order modul is added

---
+++ models/Order.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  
+  userName: {
+    type: String,
+    required: true
+  },
+
+  tableNumber: {
+    type: Number,
+    required: true
+  },
+
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+      },
+      quantity: {
+        type: Number,
+        default: 1
+      }
+    }
+  ],
+
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending"
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Order", orderSchema);
