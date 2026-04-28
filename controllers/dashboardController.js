8aa44dc8447043c89a3cd091221c28e574297280
Its33shubh <shubhamkakloter123@gmail.com>
2026-04-28 10:40:18 +0530

    update the dashboard for recent order show in dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,31 +1,48 @@
 const Product = require("../models/Product");
 const Category = require("../models/Category");
+const Order = require("../models/Order");
 
 exports.getDashboard = async (req, res) => {
-    try {
+  try {
 
-        const [totalProducts, totalCategories, recentProducts] =
-            await Promise.all([
-                Product.countDocuments(),
-                Category.countDocuments(),
-                Product.find().sort({ createdAt: -1 }).limit(5)
-            ]);
+    const [
+      totalProducts,
+      totalCategories,
+      recentProducts,
+      pendingOrders,
+      recentOrders
+    ] = await Promise.all([
+      Product.countDocuments(),
+      Category.countDocuments(),
+      Product.find().sort({ createdAt: -1 }).limit(5),
 
-        res.json({
-            error: false,
-            success: true,
-            data: {
-                totalProducts,
-                totalCategories,
-                recentProducts
-            }
-        });
+      //pending orders
+      Order.countDocuments({ status: "pending" }),
 
-    } catch (error) {
-        res.status(500).json({
-            error: true,
-            success:false,
-            message: error.message
-        });
-    }
+      //recent order 
+      Order.find()
+        .populate("items.product", "name")
+        .sort({ createdAt: -1 })
+        .limit(5)
+    ]);
+
+    res.json({
+      error: false,
+      success: true,
+      data: {
+        totalProducts,
+        totalCategories,
+        pendingOrders,   
+        recentProducts,
+        recentOrders     
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      success: false,
+      message: error.message
+    });
+  }
 };
