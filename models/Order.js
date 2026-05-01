f1c4ad0fe723b8334b16085a012c5ce061316ccb
Its33shubh <shubhamkakloter123@gmail.com>
2026-05-01 11:31:51 +0530

    add the user id to the show user id in record

--- models/Order.js
+++ models/Order.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
 
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
   userName: {
     type: String,
     required: true
